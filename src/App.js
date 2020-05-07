506c072a7c7afe569481f2d30876ee16af4f9eb0
Lockett Pundt <kalifornium@hotmail.com>
2020-05-07 13:59:51 -0400

    passing the setReport function down to the form to handle be updated when the user clicks submit. it's then passed back up and into the toxicityreport component.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 /* eslint-disable react/jsx-filename-extension */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Form from './components/Form';
 import ToxicityReport from './components/ToxicityReport';
 
 function App() {
+  const [report, setReport] = useState(null);
+
+  useEffect(() => {
+    // console.log(report);
+  }, [report]);
+
+
   return (
     <div className="App">
-      <Form />
-      <ToxicityReport />
+      <Form setReport={setReport} />
+      <ToxicityReport report={report} />
     </div>
   );
 }
