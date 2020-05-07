5832b2396cb02e47520cd0d22c8b18a3503086de
Lockett Pundt <kalifornium@hotmail.com>
2020-05-07 11:21:18 -0400

    this is render the toxicity info from the API call. it's being passed props from the form component.

---
+++ src/components/ToxicityReport.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const ToxicityReport = (props) => {
+  const [placeholder, setPlaceholder] = useState('');
+
+  return (
+    <div>
+      This is the toxicity report.
+    </div>
+  );
+};
+
+export default ToxicityReport;
