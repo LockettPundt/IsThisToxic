1cf8a3ad8a48f3f2c0b16db7eabb14a54bc02bf2
Lockett Pundt <kalifornium@hotmail.com>
2020-05-07 11:21:58 -0400

    import the toxicity report component but im going to move it to the form component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import './App.css';
+import Form from './components/Form';
+import ToxicityReport from './components/ToxicityReport';
 
 function App() {
   return (
     <div className="App">
-      <p>hi!</p>
+      <Form />
+      <ToxicityReport />
     </div>
   );
 }
