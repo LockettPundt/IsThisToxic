0f8bece6401bba71e6a4c973e4a890c81ce07c8e
Lockett Pundt <kalifornium@hotmail.com>
2020-05-08 19:06:42 -0400

    added a user input for sensitivity level that they can choose a number 1 - 10. added logic to deny anything outside of a number 1-10.

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable radix */
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
@@ -5,19 +6,32 @@ const toxicity = require('@tensorflow-models/toxicity');
 
 const Form = ({ setReport }) => {
   const [text, setText] = useState('');
+  const [sensitivity, setSensitivity] = useState('');
+  const [inputOnePlaceholder, setInputOnePlaceholder] = useState('Sensitivity Level 1 - 10');
 
   const getToxicity = async (e) => {
     e.preventDefault();
-    const threshold = await toxicity.load(0.9);
-    const toxicReport = await threshold.classify([text]);
-    // console.log(model);
-    setReport(toxicReport);
-    setText('');
+    if (sensitivity > 0 && sensitivity <= 10) {
+      const threshold = await toxicity.load(sensitivity / 10);
+      const toxicReport = await threshold.classify([text]);
+      setReport(toxicReport);
+      setText('');
+      setSensitivity('');
+      return setInputOnePlaceholder('Sensitivity Level 1 - 10');
+    }
+    setSensitivity('');
+    return setInputOnePlaceholder('Please Enter a Number 1 - 10 ');
   };
 
 
   return (
     <form onSubmit={(e) => getToxicity(e)}>
+      <input
+        type="text"
+        placeholder={inputOnePlaceholder}
+        value={sensitivity}
+        onChange={(e) => setSensitivity(e.target.value)}
+      />
       <input
         type="text"
         placeholder="Type or Paste text here"
