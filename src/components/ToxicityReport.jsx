82f5cd4e9dbb4c651c2c115ee1e9d72618ffd97d
Lockett Pundt <kalifornium@hotmail.com>
2020-05-08 20:49:31 -0400

    declared prop types.

--- src/components/ToxicityReport.jsx
+++ src/components/ToxicityReport.jsx
@@ -1,8 +1,11 @@
+/* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
 
 const ToxicityReport = ({ report }) => {
-  const [placeholder, setPlaceholder] = useState('hi');
+  const [placeholder, setPlaceholder] = useState('Hi, enter some text to analyzed for toxicity.');
 
   console.log(report);
 
@@ -11,6 +14,10 @@ const ToxicityReport = ({ report }) => {
       {report ? <p>this is the report.</p> : <p>{placeholder}</p>}
     </div>
   );
+};
+
+ToxicityReport.propTypes = {
+  report: PropTypes.arrayOf.isRequired,
 };
 
 export default ToxicityReport;
