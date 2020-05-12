0b941dc98fd53b4c74971e3fa8a6f366e5be2e39
Lockett Pundt <kalifornium@hotmail.com>
2020-05-12 11:43:49 -0400

    made a title with a styled h1.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 /* eslint-disable react/jsx-filename-extension */
-import React, { useState, useEffect } from 'react';
-import './App.css';
+import React, { useState } from 'react';
+import styled from 'styled-components';
 import Form from './components/Form';
 import ToxicityReport from './components/ToxicityReport';
+
+const Title = styled.h1`
+  text-align: center;
+  width: 200px;
+  margin: 3rem auto 0rem auto;
+`;
 
 function App() {
   const [report, setReport] = useState(null);
 
-  useEffect(() => {
-    // console.log(report);
-  }, [report]);
-
-
   return (
     <div className="App">
+      <Title>Is This Toxic?</Title>
       <Form setReport={setReport} />
       <ToxicityReport report={report} />
     </div>
